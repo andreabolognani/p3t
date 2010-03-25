885761b89bf316c8dea812ba465e917a95e524c2
Andrea Bolognani <eof@kiyuko.org>
2010-03-25 21:41:16 +0100

    Make p3t_application use p3t_timerWidgets.

--- source/p3t_application.c
+++ source/p3t_application.c
@@ -3,6 +3,7 @@
 #include <p3t_point.h>
 #include <p3t_box.h>
 #include <p3t_widget.h>
+#include <p3t_timerWidget.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,37 +14,9 @@
 #define SECONDS_PER_MINUTE 60
 
 struct _p3t_application {
-	p3t_timer   **timers;
-	p3t_widget  **widgets;
+	p3t_timer        **timers;
+	p3t_timerWidget  **widgets;
 };
-
-static void
-activateCallback (p3t_widget  *self,
-                  void        *data)
-{
-	p3t_timer *timer;
-
-	timer = (p3t_timer*) data;
-
-	switch (p3t_timerGetState (timer)) {
-
-		case P3T_TIMER_STATE_STOPPED:
-		case P3T_TIMER_STATE_PAUSED:
-
-			p3t_timerStart (timer);
-			break;
-
-		case P3T_TIMER_STATE_RUNNING:
-
-			p3t_timerPause (timer);
-			break;
-
-		case P3T_TIMER_STATE_FINISHED:
-
-			p3t_timerStop (timer);
-			break;
-	}
-}
 
 static void
 init (p3t_application *self)
@@ -51,25 +24,31 @@ init (p3t_application *self)
 	int i;
 
 	self->timers = malloc (TIMERS_NUMBER * sizeof (p3t_timer *));
-	self->widgets  = malloc (TIMERS_NUMBER * sizeof (p3t_widget *));
+	self->widgets = malloc (TIMERS_NUMBER * sizeof (p3t_timerWidget *));
 
 	/* Create all the needed timers */
 	for (i = 0; i < TIMERS_NUMBER; i++) {
 		self->timers[i] = p3t_timerNew (i + 1);
-		self->widgets[i] = p3t_widgetNew ();
-		p3t_widgetSetActivateCallback (self->widgets[i],
-		                               &activateCallback,
-		                               self->timers[i]);
+		self->widgets[i] = p3t_timerWidgetNew ();
+		p3t_timerWidgetSetTimer (self->widgets[i], self->timers[i]);
 	}
 
-	p3t_widgetSetBox (self->widgets[0], p3t_boxNew (3, 3, 124, 45));
-	p3t_widgetSetBox (self->widgets[1], p3t_boxNew (3, 50, 124, 45));
-	p3t_widgetSetBox (self->widgets[2], p3t_boxNew (3, 97, 124, 45));
-	p3t_widgetSetBox (self->widgets[3], p3t_boxNew (3, 144, 124, 45));
-	p3t_widgetSetBox (self->widgets[4], p3t_boxNew (129, 3, 124, 45));
-	p3t_widgetSetBox (self->widgets[5], p3t_boxNew (129, 50, 124, 45));
-	p3t_widgetSetBox (self->widgets[6], p3t_boxNew (129, 97, 124, 45));
-	p3t_widgetSetBox (self->widgets[7], p3t_boxNew (129, 144, 124, 45));
+	p3t_widgetSetBox (P3T_WIDGET (self->widgets[0]),
+	                  p3t_boxNew (3, 3, 124, 45));
+	p3t_widgetSetBox (P3T_WIDGET (self->widgets[1]),
+	                  p3t_boxNew (3, 50, 124, 45));
+	p3t_widgetSetBox (P3T_WIDGET (self->widgets[2]),
+	                  p3t_boxNew (3, 97, 124, 45));
+	p3t_widgetSetBox (P3T_WIDGET (self->widgets[3]),
+	                  p3t_boxNew (3, 144, 124, 45));
+	p3t_widgetSetBox (P3T_WIDGET (self->widgets[4]),
+	                  p3t_boxNew (129, 3, 124, 45));
+	p3t_widgetSetBox (P3T_WIDGET (self->widgets[5]),
+	                  p3t_boxNew (129, 50, 124, 45));
+	p3t_widgetSetBox (P3T_WIDGET (self->widgets[6]),
+	                  p3t_boxNew (129, 97, 124, 45));
+	p3t_widgetSetBox (P3T_WIDGET (self->widgets[7]),
+	                  p3t_boxNew (129, 144, 124, 45));
 }
 
 static void
@@ -79,8 +58,7 @@ finalize (p3t_application *self)
 
 	/* Destroy all timers and widgets */
 	for (i = 0; i < TIMERS_NUMBER; i++) {
-		p3t_timerDestroy (self->timers[i]);
-		p3t_widgetDestroy (self->widgets[i]);
+		p3t_timerWidgetDestroy (self->widgets[i]);
 	}
 
 	/* Destroy containers */
@@ -136,7 +114,8 @@ p3t_applicationUpdate (p3t_application *self,
 		stylus = p3t_pointNew (touch.px, touch.py);
 
 		for (i = 0; i < TIMERS_NUMBER; i++) {
-			p3t_widgetTryActivate (self->widgets[i], stylus);
+			p3t_widgetTryActivate (P3T_WIDGET (self->widgets[i]),
+			                       stylus);
 		}
 
 		p3t_pointDestroy (stylus);
