e9e32b2a0cd4bc82e0b5b849be22455ca5faafaf
Andrea Bolognani <eof@kiyuko.org>
2011-08-25 23:32:53 +0200

    Update Copyright years.

--- include/p3t_clock.h
+++ include/p3t_clock.h
@@ -1,5 +1,5 @@
 /* P3T - Coundown timers for Nintendo DS
- * Copyright (C) 2010  Andrea Bolognani <eof@kiyuko.org>
+ * Copyright (C) 2010-2011  Andrea Bolognani <eof@kiyuko.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
