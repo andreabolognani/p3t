c99f5a9ec01f602b1e77df20db47e4016a42ae65
Andrea Bolognani <eof@kiyuko.org>
2018-08-25 16:54:11 +0200

    Add Android skeleton
    Empty application generated using Android Studio.

---
+++ app/src/main/java/org/kiyuko/p3t/MainActivity.java
@@ -0,0 +1,30 @@
+/* P3T - Countdown timers for your device
+ * Copyright (C) 2010-2018  Andrea Bolognani <eof@kiyuko.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.kiyuko.p3t;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
